8923358c6a5c34e8ed35dd89abb86790f967f115
Talksxb <56055761+Talksxb@users.noreply.github.com>
2019-12-02 14:04:51 -0300

    Create blockchain.js

---
+++ javascript/blockchain.js
@@ -0,0 +1,56 @@
+var contractAddress = "0xb79e9eFc78ad1E40795fC8b93333d2224b3fA43b";
+var provider = new ethers.providers.Web3Provider(web3.currentProvider);
+var signer = provider.getSigner();
+var contract = new ethers.Contract(contractAddress, contractAbi, signer);
+
+function gettotalarrecadado() {    
+    var boxBalance = document.getElementById("boxBalance");
+    console.log("gettotalarrecadado - estamos processando a sua doação");     
+    contract.gettotalarrecadado()
+    .then( (resultFromContract) => {
+        console.log("gettotalarrecadado - o total é", resultFromContract);
+        boxBalance.innerHTML = resultFromContract;
+    })
+    .catch( (err) => {
+        console.error(err);
+        alert("Em breve, a autorização do Metamask irá surgir, pedindo para confirmar a transação");
+        ethereum.enable();
+        alert("Após a confirmação, a página será atualizada!!");
+        document.location = "index.html";
+    });
+}
+
+function executePayment() {
+    var amount = document.frmPayment.amount.value;       
+    if (amount<1000000000) {
+        alert("O valor mínimo de doação é: 1 wei!");
+        return false;
+    }
+    var doador = document.frmPayment.doador.value;
+    var boxCommStatus = document.getElementById("boxCommStatus");
+    boxCommStatus.innerHTML = "mais alguns segundos, estamos enviando a sua doação!";
+    var additionalSettings = {
+        value: ethers.utils.parseUnits(amount, 'wei')
+    }; 
+    contract.doe(doador, additionalSettings)
+    .then( (tx) => {
+        console.log("executePayment - Transaction ", tx);   
+        boxCommStatus.innerHTML = "Doação realizada com sucesso!";
+        tx.wait()
+        .then( (resultFromContract) => {
+            console.log("executePayment - o total arrecadado é", resultFromContract);
+            gettotalarrecadado();
+            boxCommStatus.innerHTML = "Doação confirmada";
+        })        
+        .catch( (err) => {
+            console.error("executePayment - after tx being mint");
+            console.error(err);
+            boxCommStatus.innerHTML = "Algo deu errado com a sua doação" + err.message;
+        })
+    })
+    .catch( (err) => {
+        console.error("executePayment - doação confirmada");
+        console.error(err);
+        boxCommStatus.innerHTML = "Algo deu errado com a sua doação " + err.message;
+    })
+}
